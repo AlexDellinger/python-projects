d3e753d00db29f8efa03e75223176d81892df7ff
alexdellinger <alexdellinger11@gmail.com>
2014-08-14 16:42:55 -0400

    moved bridge keeper 2

---
+++ bridge keeper 2.py
@@ -0,0 +1,10 @@
+name = input("what is your name")
+quest = input ("what is your quest")
+
+
+if name == 'lancelot':
+    color = input ("what is your favorite color")
+    if color == "blue":
+        print("you may pass")
+    else:
+        print ("you are thrown into the gorge of eternal peril")
