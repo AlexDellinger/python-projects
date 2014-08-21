475903f198780b13cd1f84f1765b0736296cd451
alexdellinger <alexdellinger11@gmail.com>
2014-08-21 16:57:11 -0400

    added waffles and nyan cat

---
+++ WAFFLES!!!!!!!!.py
@@ -0,0 +1,12 @@
+likes_waffles = input("♫ Do you like waffles?")
+
+if likes_waffles == 'yes':
+    print("Yeah we like waffles!")
+    likes pancakes = input("Do you like pancakes?")
+    if likes_pancakes == 'yes':
+        print("Yeah we like pancakes!")
+        if likes_french_toast == 'yes':
+            print("yeah we like french toast!")
+            for count in range(3)
+            
+
